3fc281f7459171ddbe53ba4e0c8b7b8c5a6c9e2a
DW-203 <DW-203@DESKTOP-HO8MVAN>
2026-01-08 10:02:42 +0900

    work

--- java_control/src/study20260107/SeatReservation.java
+++ java_control/src/study20260107/SeatReservation.java
@@ -29,29 +29,70 @@ public class SeatReservation {
 //		예약 인원 수 : 5
 //		연속 좌석이 없습니다
 
-		int[] seats = new int[50];
-		int res_all = 0;
+		int[] seats = {
+				1,0,0,0,0,0,0,0,1,1,
+				0,0,0,0,0,1,1,1,1,1,
+				0,1,0,1,1,0,0,0,0,0,
+				0,1,1,0,0,1,1,0,1,0,
+				0,0,0,1,1,1,0,0,0,0
+		};
+//		int res_all = 0;
 
 //		Arrays.fill(seats, 0);
 
 		while (true) {
 			System.out.print("예약 인원 수 : ");
 			int res_num = sc.nextInt();
+			
+//			예약인원이 없으면 좌석현황을 출력하고 프로그램 종료
+			if (res_num == 0) {
+				System.out.println("좌석 현황 : ");
+				for (int seat : seats) {
+					if (seat == 0) System.out.print("□");
+					else System.out.print("■");
+				}
+				break;
+			}
+			
+//			version 1
 
 //			예약 신청 인원 수 : res_num
 //			예약된 인원 수(마지막 예약자 좌석번호) : res_all + 1 (seats 배열의 0번 인덱스 = 1번 좌석)
 //			전체 좌석 수 : seats.length
 //			남은 자리 수 : seats.length - (res_all + 1)
 //			남은 자리수보다 예약 인원수가 작아야 예약이 가능하다
-			if (res_all + res_num < seats.length) {
-				for (int i = 0; i < res_num; i++) {
-					seats[i + res_all] = 1;
+			
+//			if (res_all + res_num < seats.length) {
+//				for (int i = 0; i < res_num; i++) {
+//					seats[i + res_all] = 1;
+//				}
+//				System.out.println(res_num + "석 예약 완료 (" + (res_all + 1) + "번 ~ " + (res_all + res_num) + "번)");
+//				res_all = res_all + res_num;
+//			} else {
+//				System.out.println("연속 좌석이 없습니다.");
+//				break;
+//			}
+			
+			
+//			원하는 좌석 위치를 입력받아 그 위치부터 인원수만큼 예약
+			
+			System.out.print("원하는 위치 입력 : ");
+//			int line = sc.nextInt() - 1;
+			int pos = sc.nextInt() - 1;
+			boolean isOk = true;
+			
+			for (int i = 0; i < res_num; i++) {
+				if (seats[pos + i] == 1) {
+					System.out.println("연속 좌석이 없습니다.");
+					isOk = false;
+					break;
 				}
-				System.out.println(res_num + "석 예약 완료 (" + (res_all + 1) + "번 ~ " + (res_all + res_num) + "번)");
-				res_all = res_all + res_num;
-			} else {
-				System.out.println("연속 좌석이 없습니다.");
-				break;
+			}
+			if (isOk) {
+				for (int i = 0; i < res_num; i++) {
+					seats[pos + i] = 1;
+				}
+				System.out.println(res_num + "석 예약 완료 (" + (pos + 1) + "번 ~ " + (pos + res_num) + "번)");
 			}
 		}
 
