fbc5a9b46b9919590f2437bacb35647dfcc2bd70
mingun <mingun@mingun>
2026-01-13 20:52:23 +0900

    aa

---
+++ java_control/src/study20260112/Product.java
@@ -0,0 +1,66 @@
+package study20260112;
+
+public class Product {
+
+	private String name;
+	private int price;
+	private String category;
+	private int amount;
+	
+	
+	public Product(String name, int price, String category, int amount) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.amount = amount;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + ", category=" + category + ", amount=" + amount + "]";
+	}
+	
+	
+}
---
+++ java_control/src/study20260112/User.java
@@ -0,0 +1,54 @@
+package study20260112;
+
+public class User {
+	
+	private String name;
+	private String id;
+	private String password;
+	private String phoneNumber;
+	
+	public User(String name, String id, String password, String phoneNumber) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", id=" + id + ", password=" + password + ", phoneNumber=" + phoneNumber + "]";
+	}
+}
